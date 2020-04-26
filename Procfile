0ee081ec3fca610e953777f19b78548c9e2da0f9
Varkha Yadav <varkhay@gmail.com>
2020-04-26 21:32:57 +0530

    deploying to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:gunicorn lyricsengine.wsgi --log-file--
--- lyricsengine/settings.py
+++ lyricsengine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_o#%+^bm!(k_42vzb$2&8q_llf9drh@ml0#6z-6dt8ij$##*i%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://lyricsengine.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
